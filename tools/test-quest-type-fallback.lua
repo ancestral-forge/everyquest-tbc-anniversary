a7fa7e589a7262269baf8b8b7fbfd93a0c0418ec
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2026-08-22 11:52:18 +0300

    Handle unknown quest type tags

---
+++ tools/test-quest-type-fallback.lua
@@ -0,0 +1,20 @@
+local sourceFile = assert(io.open("EveryQuest/Everyquest.lua", "r"))
+local source = sourceFile:read("*a")
+sourceFile:close()
+
+EveryQuest = {}
+_G.L = setmetatable({}, {
+	__index = function(_, key)
+		return key
+	end,
+})
+
+local questTypeSource = assert(source:match("(function EveryQuest:QuestType.-\nend)\n"))
+assert(loadstring(questTypeSource))()
+assert(EveryQuest:QuestType(84) == "", "unknown quest types must use an empty display tag")
+assert(EveryQuest:QuestType(9999) == "", "future unknown quest types must use an empty display tag")
+
+local updateButtonSource = assert(source:match("(function EveryQuest:UpdateButton.-)\nfunction EveryQuest:ButtonEnter"))
+assert(updateButtonSource:find('self:QuestType%(quest%["t"%]%) or ""'), "renderer must guard QuestType results")
+
+print("Quest type fallback test passed.")
